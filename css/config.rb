87aea18dd4aade9d1bc8162b2858c65c0351cd6e
Shajed Evan <evanshajed@gmail.com>
2001-01-01 06:05:36 +0600

    Final 27-12-12

--- css/config.rb
+++ css/config.rb
@@ -4,8 +4,8 @@
 http_path = "/"
 css_dir = "stylesheets"
 sass_dir = "sass"
-images_dir = "../img"
-javascripts_dir = "../js"
+images_dir = "/img"
+javascripts_dir = "/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
@@ -15,8 +15,6 @@ javascripts_dir = "../js"
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
-
-
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
 # preferred_syntax = :sass
